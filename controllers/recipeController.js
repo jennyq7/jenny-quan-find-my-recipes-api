9611ca4203a4655cab9bc8c602d431743c0812cd
codingisfun <61808280+jennyq7@users.noreply.github.com>
2022-12-08 14:34:53 -0500

    work on set up tables

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 exports.index = async( req, res) => { 
     await axios.get('https://platform.fatsecret.com/rest/server.api?method=recipes.search.v2&format=json&max_results=50', {headers: {
-    Authorization: "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjVGQUQ4RTE5MjMwOURFRUJCNzBCMzU5M0E2MDU3OUFEMUM5NjgzNDkiLCJ0eXAiOiJhdCtqd3QiLCJ4NXQiOiJYNjJPR1NNSjN1dTNDeldUcGdWNXJSeVdnMGsifQ.eyJuYmYiOjE2NzAzNzY2NzgsImV4cCI6MTY3MDQ2MzA3OCwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjoiYmFzaWMiLCJjbGllbnRfaWQiOiI3Njk3NWEzYzNmZTI0N2QyOTkxZGE4ZjEwNmM4YzhiZiIsInNjb3BlIjpbImJhc2ljIl19.fwh44PBGXNBjCYbCgqSEtO_g0kC3bQOgX-Wz_nqqU856pVafB5xmoyz9qkNAbv6lWpOOrozdaSWpahJUKUMcKD7kQV-Z5rZSLGjRJfDiQ77-1x79v3dXY9fzTiZGhVQSgpJAtMh-i0a5k96-hwC2kM530AZs9WeP6QMOb2N7SoNWrs0U_qvux6Qmy1BlLA8repMAoi312LLAlGa3YqPeu3wKqf1QMD5lWFx3me_9x9jZ6XZNojZfq5g8lmLaSLnQOgAlJPTe85NNsAj3JNc14wqc3py6vgOoQ-vMHN05xgjUM_FLFg7RwVTqZ8okI1qcJj8w48oTVfrxrRMk3vfiGA"
+    Authorization: "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjVGQUQ4RTE5MjMwOURFRUJCNzBCMzU5M0E2MDU3OUFEMUM5NjgzNDkiLCJ0eXAiOiJhdCtqd3QiLCJ4NXQiOiJYNjJPR1NNSjN1dTNDeldUcGdWNXJSeVdnMGsifQ.eyJuYmYiOjE2NzA0NzA5NTgsImV4cCI6MTY3MDU1NzM1OCwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjoiYmFzaWMiLCJjbGllbnRfaWQiOiI3Njk3NWEzYzNmZTI0N2QyOTkxZGE4ZjEwNmM4YzhiZiIsInNjb3BlIjpbImJhc2ljIl19.pZeRxbcEv4Gjens2P59soOEtG_NpB1EFcS9iyz5RCJVWjUIIKQkmukoMwQughvNY5KsnBjnObqQ2_godGfUPw-Uxl2yRTqBUz26t7SlE94TgyBbhVXL4CscmwS88-baOCDtLULf9U0kZZS5__Fi8SixbyeEahXblWbolUL5eG4rT41LhIcHklmLs_2iiE3geNpYWRwEBLDB6l7Y9aL2swWJbmk703uMi6XS_o0zQWP93y1YtwIi7SeNdXY-wRnUZPWwL6b4vplAiC1_CW5HkOR5wdBbgJUZyqL-lX3dpR2_XjRzCuhP0DJzWxZkdsBvODGAxoaVV9U1zAtUMCalD-Q"
 }}    
 )
 .then(response => {
@@ -11,3 +11,5 @@ exports.index = async( req, res) => {
 
 }).catch (err => {console.log(err)} )
 }
+
+
--- migrations/20221204205705_create-recipes-table.js
+++ migrations/20221204205705_create-recipes-table.js
@@ -4,52 +4,13 @@
  */
 exports.up = function(knex) {
   return knex.schema.createTable('recipe', (table) => {
-    table.uuid('idMeal').primary();
-    table.string('strMeal').notNullable();
-    table.string('strCategory').notNullable();
-    table.string('strArea').notNullable();
+    table.uuid('recipe_id').primary();
+    table.string('recipe_name').notNullable();
+    table.string('recipe_types').notNullable();
+    table.string('recipe_description').notNullable();
     table.string('strInstuctions').notNullable();
-    table.string('strMealThumb').notNullable();
-    table.string('strIngredient1')
-    table.string('strIngredient2')
-    table.string('strIngredient3')
-    table.string('strIngredient4')
-    table.string('strIngredient5')
-    table.string('strIngredient6')
-    table.string('strIngredient7')
-    table.string('strIngredient8')
-    table.string('strIngredient9')
-    table.string('strIngredient10')
-    table.string('strIngredient11')
-    table.string('strIngredient12')
-    table.string('strIngredient13')
-    table.string('strIngredient14')
-    table.string('strIngredient15')
-    table.string('strIngredient16')
-    table.string('strIngredient17')
-    table.string('strIngredient18')
-    table.string('strIngredient19')
-    table.string('strIngredient20')
-    table.string('strMeasure1')
-    table.string('strMeasure2')
-    table.string('strMeasure3')
-    table.string('strMeasure4')
-    table.string('strMeasure5')
-    table.string('strMeasure6')
-    table.string('strMeasure7')
-    table.string('strMeasure8')
-    table.string('strMeasure9')
-    table.string('strMeasure10')
-    table.string('strMeasure11')
-    table.string('strMeasure12')
-    table.string('strMeasure13')
-    table.string('strMeasure14')
-    table.string('strMeasure15')
-    table.string('strMeasure16')
-    table.string('strMeasure17')
-    table.string('strMeasure18')
-    table.string('strMeasure19')
-    table.string('strMeasure20')
+    table.string('recipe_image').notNullable();
+    table.string
   });
 };
 
@@ -60,3 +21,5 @@ exports.up = function(knex) {
 exports.down = function(knex) {
   return knex.schema.dropTable('recipe');
 }; 
+
+
