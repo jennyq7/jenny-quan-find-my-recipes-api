8042435cf1ab0518c14a32871efd3acca04efaf0
codingisfun <61808280+jennyq7@users.noreply.github.com>
2023-04-09 10:42:55 -0400

    updated knex

--- knexfile.js
+++ knexfile.js
@@ -6,11 +6,21 @@
 module.exports = {
   client: 'mysql',
   connection: {
-    host: 'mysql-kpiq',
+    host: 'recipe-box-db',
     charset: 'utf8',
     database: "mysql",
-    user: "mysql",
-    password: "Td0WgfWWHBjdbmMkcWuNgcUiqz2S15bC"
+    user: "jenny",
+    password: "ARqTLhgv6KXF6BA!"
    }
 };
 
+// module.exports = {
+//   client: 'mysql',
+//   connection: {
+//     host: 'mysql-kpiq',
+//     charset: 'utf8',
+//     database: "mysql",
+//     user: "mysql",
+//     password: "Td0WgfWWHBjdbmMkcWuNgcUiqz2S15bC"
+//    }
+// };
