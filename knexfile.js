70333cc6c3143cf75f3357311bfc3b7bc905bada
codingisfun <61808280+jennyq7@users.noreply.github.com>
2023-05-08 21:53:43 -0400

    restart azure

--- knexfile.js
+++ knexfile.js
@@ -4,25 +4,25 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 //db for azure project
-// module.exports = {
-//   client: 'mysql',
-//   connection: {
-//     host: 'mysqltestdb123.mysql.database.azure.com',
-//     charset: 'utf8',
-//     database: "quickstartdb",
-//     user: "mysqluser1",
-//     password: "T*6g2xDF%"
-//    }
-// };
-//
-//db for Render deployment
 module.exports = {
   client: 'mysql',
   connection: {
-    host: 'mysql-kpiq',
+    host: 'mysqltestdb123.mysql.database.azure.com',
     charset: 'utf8',
-    database: "mysql",
-    user: "mysql",
-    password: "Td0WgfWWHBjdbmMkcWuNgcUiqz2S15bC"
+    database: "quickstartdb",
+    user: "mysqluser1",
+    password: "T*6g2xDF%"
    }
 };
+//
+//db for Render deployment
+// module.exports = {
+//   client: 'mysql',
+//   connection: {
+//     host: 'mysql-kpiq',
+//     charset: 'utf8',
+//     database: "mysql",
+//     user: "mysql",
+//     password: "Td0WgfWWHBjdbmMkcWuNgcUiqz2S15bC"
+//    }
+// };
